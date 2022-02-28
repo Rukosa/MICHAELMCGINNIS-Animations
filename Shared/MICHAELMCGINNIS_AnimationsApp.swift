59ca102618176bffd73d9003d8f57462c6ce2638
Rukosa <mwmcginnis490@gmail.com>
2022-02-28 14:38:59 -0800

    Initial Commit

---
+++ Shared/MICHAELMCGINNIS_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MICHAELMCGINNIS_AnimationsApp.swift
+//  Shared
+//
+//  Created by Michael Mcginnis on 2/28/22.
+//
+
+import SwiftUI
+
+@main
+struct MICHAELMCGINNIS_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
